c968a38a178062f5e702e935224d0531fa57b621
Henrique Poncetti Zegalo <henrique.ze@live.com>
2019-04-19 20:45:03 -0300

    add tasks on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(title: 'Estudar Rails', done: false)
+Task.create(title: 'Soldar componentes', done: false)
+Task.create(title: 'Comprar Pasta de Solda', done: true)
+Task.create(title: 'Estudar React', done: true)
