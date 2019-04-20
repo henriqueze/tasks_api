252d7e3adcd0b0c3e83d9f8caa4d8b1b541f7a10
Henrique Poncetti Zegalo <henrique.ze@live.com>
2019-04-19 22:27:48 -0300

    modify css to scss

--- tasks_client/src/App.js
+++ tasks_client/src/App.js
@@ -1,32 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheckCircle, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
-
-library.add(faCheckCircle, faTrashAlt)
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
-
-export default App;
+   import './App.scss';
+   import Header from './components/header/Header';
+   
+   import { library } from '@fortawesome/fontawesome-svg-core'
+   import { faCheckCircle, faTrashAlt } from '@fortawesome/free-regular-svg-icons'
+   
+   library.add(faCheckCircle, faTrashAlt)
+   
+   class App extends Component {
+     render() {
+       return (
+         <div>
+           <Header/>
+         </div>
+       );
+     }
+   }
+   
+   export default App;
