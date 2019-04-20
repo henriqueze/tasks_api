0215bf6f17334bd6780975aca96df1a788f8cc46
Henrique Poncetti Zegalo <henrique.ze@live.com>
2019-04-19 23:00:11 -0300

    list.js

--- tasks_client/src/components/tasks/list/List.js
+++ tasks_client/src/components/tasks/list/List.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+   import Row from 'react-bootstrap/Row';
    import Card from 'react-bootstrap/Card';
+   import Table from 'react-bootstrap/Table';
+   import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
    
    class List extends Component {
      render() {
@@ -7,7 +10,29 @@ import React, { Component } from 'react';
          <div>
            <Card>
              <Card.Body>
-               Table...
+             <Table responsive>
+               <tbody>
+                 {this.props.tasks.map((task, index) => {
+                   return <tr key={task.id}>
+                     <td className="col-md-10">{task.title}</td>
+                     <td>
+                       { 
+                         task.done == false
+                         ? <a className="check" href="#">
+                             <FontAwesomeIcon icon={['far', 'check-circle']} size="lg"/>
+                           </a> 
+                         : null
+                       }
+                     </td>
+                     <td>
+                       <a className="delete" href="#">
+                         <FontAwesomeIcon icon={['far', 'trash-alt']}/>
+                       </a>
+                     </td>
+                   </tr>;
+                 })}
+               </tbody>
+             </Table>
              </Card.Body>
            </Card>
          </div>
